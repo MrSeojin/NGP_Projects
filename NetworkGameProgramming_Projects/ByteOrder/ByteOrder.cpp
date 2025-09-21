cf3a49c42baaf38f0c149ac836fe64f14d4c0a12
Seojin.K <7701seojin@gmail.com>
2025-09-21 19:07:41 +0900

    호스트 바이트 정렬 방식 확인
    little-endian
    네트워크 바이트 정렬 방식과 호스트 바이트 정렬 방식이 다름

---
+++ NetworkGameProgramming_Projects/ByteOrder/ByteOrder.cpp
@@ -0,0 +1,25 @@
+#include "..\Common.h"
+
+u_short x1 = 0x1234;        // 호스트 바이트
+u_short x2 = htons(x1);     // 네트워크 바이트
+
+void GetHostEndian();
+
+int main() {
+    printf("[호스트 바이트 -> 네트워크 바이트 (빅 엔디언)]\n");
+    printf("%#x -> %#x\n", x1, x2);
+    printf("\n");
+
+    printf("[호스트 바이트 정렬]\n");
+    GetHostEndian();
+
+    return 0;
+}
+
+void GetHostEndian()
+{
+    if (x1 == x2)
+        printf("Big-endian\n");
+    else
+        printf("Little-endian\n");
+}
